a0327898c7a630b10c4d0490b32ea2c05c12cad4
zeyad al-houssainy <zeyad-houssainy@hotmail.com>
2024-04-16 17:16:06 +0200

    Rest

---
+++ 0x01-react_intro/task_test_deploy/dashboard/src/Notifications.test.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Notifications from "./Notifications";
+
+// describe("App Component tests", () => {
+//   it("Runs without crash", () => {
+//     const app = shallow(<App />);
+//     expect(app).toBeDefined();
+//   });
+
+//   it("App renders a div with the class App-header", () => {
+//     const app = shallow(<App />);
+//     expect(app.find(".App-header")).toBeDefined();
+//   });
+
+// });
+
+describe("Notifications Component tests", () => {
+    it("Runs without crash", () => {
+        const notifications = shallow(<Notifications />)
+        expect(notifications).toBeDefined()
+    });
+
+    it("un-organized list is defined", () => {
+        const notifications = shallow(<notifications />)
+        expect(notifications.find("ul")).toBeDefined()
+    })
+    
+    it("renders three list items", () => {
+        const notifications = shallow(<Notifications />);
+        expect(notifications.find("li")).toHaveLength(3);
+    });
+    
+    it("renders the text Here is the list of notifications", () => {
+        const notifications = shallow(<Notifications />);
+        expect(notifications.find("p").text())
+        .toBe("Here is the list of notifications");
+    });
+});
---
+++ 0x01-react_intro/task_test_deploy/dashboard/src/utils.test.js
@@ -0,0 +1,22 @@
+import { getFooterCopy, getLatestNotification, getFullYear } from "./utils";
+
+// Testing getFooterCopy function
+test('if index is true', () => { 
+  expect(getFooterCopy(true)).toBe("Holberton School");
+})
+
+test("if index is False", () => {
+  expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
+});
+
+// Testing getFooterCopy function
+test("Get the current year value", () => {
+ expect(getFullYear()).toBe(2024);
+})
+
+// Testing getFooterCopy function
+test("Function get specific value", () => {
+  expect(getLatestNotification()).toBe(
+    "<strong>Urgent requirement</strong> - complete by EOD"
+  );
+})
