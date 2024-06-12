177c0b2716422e75d58024468e5aef6da473a3c5
zeyad al-houssainy <zeyad-houssainy@hotmail.com>
2024-06-12 12:08:47 +0300

    fast_commit

--- 0x02-react_props/task_4/dashboard/src/App/App.js
+++ 0x02-react_props/task_4/dashboard/src/App/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Login from '../Login/Login';
-import Notifications from '../Notifications/Notifications';
-import './App.css';
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import Notifications from "../Notifications/Notifications";
+import "./App.css";
+import CourseList from "../CourseList/CourseList";
+import PropTypes from "prop-types";
 
-const App = () => (
-	// Class name app was not added
-	<>
-		<Notifications />
-		<Header />
-		<Login />
-		<Footer />
-	</>
+
+const App = ({ isLoggedIn }) => (
+  // Class name app was not added
+  <>
+    <Notifications />
+    <Header />
+    {isLoggedIn ? <CourseList /> : <Login />}
+    <Footer />
+  </>
 );
 
-export default App;
 
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+export default App;
